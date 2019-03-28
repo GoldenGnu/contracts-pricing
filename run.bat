8f6194d843248fe2f227a6b6485c222b22ec0a07
GoldenGnu <GoldenGnu@users.noreply.github.com>
2019-03-28 19:24:29 +0100

    2.0.0

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 rmdir /s /q %~dp0src
 rmdir /s /q %~dp0gradle
-java -jar openapi-generator-cli.jar  generate -i https://api.swaggerhub.com/apis/bahrmichael/contracts-appraisal/1.0.0/swagger.json -g java -c config.json
+java -jar openapi-generator-cli.jar  generate -i https://api.swaggerhub.com/apis/bahrmichael/contracts-appraisal/2.0.0 -g java -c config.json
 pause
