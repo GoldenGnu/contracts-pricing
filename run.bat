2c1d8cad37415a556ef23131ca418e57c4d75bd4
GoldenGnu <GoldenGnu@users.noreply.github.com>
2019-04-27 12:03:37 +0200

    rihanshazih 1.2.0

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 rmdir /s /q %~dp0src
 rmdir /s /q %~dp0gradle
-java -jar openapi-generator-cli.jar  generate -i https://api.swaggerhub.com/apis/rihanshazih/contracts-appraisal/1.1.0/swagger.json -g java -c config.json
+java -jar openapi-generator-cli.jar  generate -i https://api.swaggerhub.com/apis/rihanshazih/contracts-appraisal/1.2.0/swagger.json -g java -c config.json
 pause
